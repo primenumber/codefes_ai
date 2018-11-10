0df222799d1b01d420c0e35c3c93d931a6da948b
prime number <prime@kmc.gr.jp>
2018-11-11 02:35:57 +0900

    Implement get_combination

--- utils.cpp
+++ utils.cpp
@@ -1,7 +1,9 @@
 #include "utils.hpp"
 #include <algorithm>
+#include <vector>
 
 int comb[26][26];
+std::vector<std::vector<std::vector<uint32_t>>> table;
 
 void init() {
   for (int i = 0; i <= 25; ++i) {
@@ -15,8 +17,26 @@ void init() {
       comb[i][j] = res;
     }
   }
+  table.resize(26);
+  for (int i = 1; i <= 25; ++i) {
+    table[i].resize(i+1);
+    for (int j = 1; j <= i; ++j) {
+      table[i][j].resize(comb[i][j]);
+      int x, y;
+      int index = 0;
+      for (int T = (1 << j) - 1; T < (1 << i);
+          x = T & -T, y = T + x, T = (((T & ~y) / x) >> 1) | y) {
+        table[i][j][index] = T;
+        ++index;
+      }
+    }
+  }
 }
 
 int combination(const int n, const int k) {
   return comb[n][k];
+}
+
+uint32_t get_combination(const int n, const int k, const int index) {
+  return table[n][k][index];
 }
--- utils.hpp
+++ utils.hpp
@@ -1,5 +1,7 @@
 #pragma once
+#include <cstdint>
 
 void init();
 
 int combination(const int n, const int k);
+uint32_t get_combination(const int n, const int k, const int index);
