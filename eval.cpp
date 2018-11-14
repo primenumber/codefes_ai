869b61636f9b1b84bc2aca57dbeea8ab7687ea13
prime <prime@kmc.gr.jp>
2018-11-14 23:28:59 +0900

    Implement table based eval func

--- eval.cpp
+++ eval.cpp
@@ -1,6 +1,24 @@
 #include "eval.hpp"
+#include <fstream>
 
 constexpr int INF = 1000000;
+
+float val_table_me[16][16];
+float val_table_op[16][16];
+
+void eval_init() {
+  std::ifstream ifs("table");
+  for (int i = 0; i < 16; ++i) {
+    for (int j = 0; j <= i; ++j) {
+      ifs >> val_table_me[i][j];
+    }
+  }
+  for (int i = 0; i < 16; ++i) {
+    for (int j = 0; j <= i; ++j) {
+      ifs >> val_table_op[i][j];
+    }
+  }
+}
 
 bool is_stable(const line::Line &line) {
   bool ok = true;
@@ -61,18 +79,78 @@ int eval_impl(const Board &bd) {
   int sum = 0;
   for (int i = 0; i < 5; ++i) {
     for (int j = 0; j < 4; ++j) {
-      sum -= abs((1 << bd.at(i, j)) - (1 << bd.at(i, j+1)));
+      int a = bd.at(i, j);
+      int b = bd.at(i, j+1);
+      int x = std::max(a, b);
+      int y = std::min(a, b);
+      sum += val_table_me[x][y];
     }
   }
   for (int i = 0; i < 4; ++i) {
     for (int j = 0; j < 5; ++j) {
-      sum -= abs((1 << bd.at(i, j)) - (1 << bd.at(i+1, j)));
+      int a = bd.at(i, j);
+      int b = bd.at(i+1, j);
+      int x = std::max(a, b);
+      int y = std::min(a, b);
+      sum += val_table_me[x][y];
     }
   }
-  //sum += stability(bd);
+  return sum;
+}
+
+int eval_impl_op(const Board &bd) {
+  if (bd.is_gameover()) return -INF;
+  int sum = 0;
+  for (int i = 0; i < 5; ++i) {
+    for (int j = 0; j < 4; ++j) {
+      int a = bd.at(i, j);
+      int b = bd.at(i, j+1);
+      int x = std::max(a, b);
+      int y = std::min(a, b);
+      sum += val_table_op[x][y];
+    }
+  }
+  for (int i = 0; i < 4; ++i) {
+    for (int j = 0; j < 5; ++j) {
+      int a = bd.at(i, j);
+      int b = bd.at(i+1, j);
+      int x = std::max(a, b);
+      int y = std::min(a, b);
+      sum += val_table_op[x][y];
+    }
+  }
+  return sum;
+}
+
+int eval_diff_impl(const Board &bd) {
+  int sum = 0;
+  for (int i = 0; i < 5; ++i) {
+    for (int j = 0; j < 4; ++j) {
+      int a = bd.at(i, j);
+      int b = bd.at(i, j+1);
+      int x = std::max(a, b);
+      int y = std::min(a, b);
+      sum -= (1 << x) - (1 << y);
+    }
+  }
+  for (int i = 0; i < 4; ++i) {
+    for (int j = 0; j < 5; ++j) {
+      int a = bd.at(i, j);
+      int b = bd.at(i+1, j);
+      int x = std::max(a, b);
+      int y = std::min(a, b);
+      sum -= (1 << x) - (1 << y);
+    }
+  }
   return sum;
 }
 
 int eval(const GameState &gs) {
-  return eval_impl(gs.get_me()) - eval_impl(gs.get_op());
+  return eval_impl(gs.get_me()) + eval_impl_op(gs.get_op());
+}
+
+int eval_diff(const GameState &gs) {
+  if (gs.get_me().is_gameover()) return -INF;
+  if (gs.get_op().is_gameover()) return INF;
+  return eval_diff_impl(gs.get_me()) - eval_diff_impl(gs.get_op());
 }
--- eval.hpp
+++ eval.hpp
@@ -1,4 +1,6 @@
 #pragma once
 #include "gamestate.hpp"
 
+void eval_init();
 int eval(const GameState &gs);
+int eval_diff(const GameState &gs);
